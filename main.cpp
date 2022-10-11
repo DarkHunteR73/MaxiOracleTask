840764be2ac7f0f76f1ce1c85fca65a105a5ccd3
DarkHunteR7 <darkstranger153@gmail.com>
2022-10-11 18:12:18 +0300

    argv & trailing spaces

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 #include <memory>
 
 int main(int argc, char* argv[]) {
-    string arg { "-s" };
+    SortingType sortingType;
 
     if (argc > 1) {
-        arg = argv[1];
+        for (int i = 0; i < argc; ++i) {
+            if (static_cast<string>(argv[i]) == "-d") {
+                sortingType = sortByCount;
+            } else if (static_cast<string>(argv[i]) == "-s") {
+                sortingType = sortByAddress;
+            }
+        }
     }
-    
+
 
     const string userName { "maxi_task_db" };
     const string password { "73501505" };
@@ -17,20 +23,12 @@ int main(int argc, char* argv[]) {
     std::unique_ptr<dataAccessLayer> dal { new dataAccessLayer(userName,password,db) };
     vector<Record> res;
 
-    try
-    {
-        res = dal->getSortedQuery(arg);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-        return EXIT_FAILURE;
-    }
-    
+    res = dal->getSortedQuery(sortingType);
+
     for (auto &&i : res)
     {
         std::cout << i.address << '\t' << i.count << std::endl;
     }
-    
+
     return EXIT_SUCCESS;
 }
